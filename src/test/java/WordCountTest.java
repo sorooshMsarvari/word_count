4c136679cb13629572a7520c613f68a38be16ba6
Soroosh Mirzasarvari <sarvisoroosh@gmail.com>
2022-04-15 19:37:11 +0430

    Enhance testSimpleReadFromFile with better abstraction

--- src/test/java/WordCountTest.java
+++ src/test/java/WordCountTest.java
@@ -2,10 +2,7 @@ import org.junit.Before;
 import org.junit.Test;
 import word_count.WordCount;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 import static org.junit.Assert.*;
 
@@ -22,10 +19,23 @@ public class WordCountTest {
 
     @Test
     public void testSimpleReadFromFile() throws IOException {
-        InputStream inputFileStream = new FileInputStream("src/test/java/testFile.txt");
+        String testStr = "hello this is a test";
         Integer testWordCount = 5;
+        var filePath = makeTestFileAndGetPath(testStr);
+        FileInputStream inputFileStream = new FileInputStream(filePath);
+
         WordCount unitUnderTest = new WordCount(inputFileStream);
         assertEquals(testWordCount, unitUnderTest.getWordCount());
         inputFileStream.close();
+    }
+
+    private String makeTestFileAndGetPath(String testString) throws IOException {
+        final String FILE_LOCATION = "src/test/java/testFile.txt";
+        File fileObj = new File(FILE_LOCATION);
+        fileObj.createNewFile();
+        FileWriter writer = new FileWriter(fileObj);
+        writer.write(testString);
+        writer.close();
+        return FILE_LOCATION;
     }
 }
